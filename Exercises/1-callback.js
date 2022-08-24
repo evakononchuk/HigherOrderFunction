b712215b7b58dc7d52ad1697be5c01ae893fc7e9
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-08-24 22:37:28 +0300

    repo with tasks #8

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,11 @@
-'use strict';
+"use strict";
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  const items = Object.keys(obj);
+  for (const key of items) {
+    const value = obj[key];
+    callback(key, value, obj);
+  }
+};
 
 module.exports = { iterate };
--- Exercises/2-closure.js
+++ Exercises/2-closure.js
@@ -1,5 +1,5 @@
-'use strict';
+"use strict";
 
-const store = x => null;
+const store = (x) => () => x;
 
 module.exports = { store };
